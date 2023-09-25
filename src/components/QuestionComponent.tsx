245e99fa6c99093811c33f2532212365f6985e76
jdwdonga21 <ongame21212144@gmail.com>
2023-09-25 16:55:04 +0900

    기능완성

--- src/components/QuestionComponent.tsx
+++ src/components/QuestionComponent.tsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import { MD5 } from 'crypto-js';
 import { useNavigate } from 'react-router-dom';
+import { CSSProperties } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Q1img from '../assets/Q1.png'
+import Q2img from '../assets/Q2.jpg'
+import Q3img from '../assets/Q3.jpeg'
 
 const questions = [
   "질문 1: 가성비 또는 가심비?",
   "질문 2: 관광지마스터 또는 숙소올인형?",
   "질문 3: 뚜벅이 또는 드라이브?",
+];
+const questionImg = [
+    Q1img,
+    Q2img,
+    Q3img,
 ];
 
 interface State {
@@ -41,12 +57,48 @@ class QuestionComponent extends Component<Props, State> {
 
     if (step < questions.length) {
       const options = questions[step].split(":")[1].split("또는").map((s) => s.trim());
+      const cardImg = questionImg[step];
       return (
-        <div>
-          <p>{questions[step]}</p>
-          <button onClick={() => this.handleAnswer(options[0])}>{options[0]}</button>
-          <button onClick={() => this.handleAnswer(options[1])}>{options[1]}</button>
-        </div>
+        <div style={styles.centerContainer}>
+            <h1>나의 KQ 숙소 유형 키워드</h1>
+            <Card sx={{ maxWidth: 400 }}>
+                <CardMedia
+                    sx={{ height: 200 }}
+                    image= {cardImg}
+                    title="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        <h4>{questions[step]}</h4>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        설명설명 설명설명설명 설명설명설명 설명설명설명 설명설명설명 설명
+                        설명설명 설명설명설명 설명설명설명 설명설명설명 설명설명설명 설명
+                        설명설명 설명설명설명 설명설명설명 설명설명설명 설명설명설명 설명설명설명 설명
+                    </Typography>
+                </CardContent>
+                <CardActions 
+                    style={{ 
+                        justifyContent: 'space-around' 
+                    }}
+                >
+                    <Button 
+                        variant="contained"
+                        size="large"
+                        onClick={() => this.handleAnswer(options[0])}
+                    >
+                        {options[0]}
+                    </Button>
+                    <Button 
+                        variant="contained"
+                        size="large"
+                        onClick={() => this.handleAnswer(options[1])}
+                    >
+                        {options[1]}
+                    </Button>
+                </CardActions>
+            </Card>
+        </div>        
       );
     }
 
@@ -60,4 +112,13 @@ const QuestionComponentWrapper: React.FC = () => {
   return <QuestionComponent navigate={navigate} />;
 };
 
+const styles: { centerContainer: CSSProperties } = {
+  centerContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // use vh for full viewport height
+  },
+};
 export default QuestionComponentWrapper;
--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -1,5 +1,33 @@
 import React, { Component } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import { CSSProperties } from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+//이미지
+import R11 from '../assets/11.jpeg';
+import R12 from '../assets/12.jpeg';
+import R21 from '../assets/21.jpg';
+import R22 from '../assets/22.jpg';
+import R31 from '../assets/31.jpg';
+import R32 from '../assets/32.jpeg';
+//숙소 이미지
+import H1 from '../assets/H1.jpg';
+import H2 from '../assets/H2.jpg';
+import H3 from '../assets/H3.jpg';
+
+import { Typography } from '@mui/material';
+
+const resultImg = [
+    [R11, R12,],
+    [R21, R22,],
+    [R31, R32,],
+];
 
 interface ResultScreenProps {
   hash: string;
@@ -9,69 +37,218 @@ interface ResultScreenProps {
 class ResultScreen extends React.Component<ResultScreenProps> {
   render() {
     const { hash } = this.props;
+  
+    let firResultImg;
+    let secResultImg;
+    let trdResultImg; 
+    let firResultText;
+    let secResultText;
+    let trdResultText; 
 
-    if (hash === "5e901317337e58ac4695c38adf172bd1") {
-      return (
-        <div>
-            <p>1Display for someHashValue</p>
-            <p>가성비 - 관광지 마스터 - 뚜벅이</p>
-        </div>
-      )
-      
-    } else if (hash === "83873ecd67c7cef8aad8f4b8169f8579") {
-      return (
-        <div>
-            <p>2Display for someHashValue</p>
-            <p>가성비 - 관광지 마스터 - 드라이브</p>
-        </div>
-      )
-    } else if (hash === "8d4328c8b65023e4551acae6071d82fd") {
-      return (
-        <div>
-            <p>3Display for someHashValue</p>
-            <p>가성비 - 숙소 올인형 - 뚜벅이</p>
-        </div>
-      )
-    } else if (hash === "5b40c94e1382312acb1b856e64759e19") {
-      return (
-        <div>
-            <p>4Display for someHashValue</p>
-            <p>가성비 - 숙소 올인형 - 드라이브</p>
-        </div>
-      )
-    } else if (hash === "d938c442280818e9d3ca7808d3b65bd5") {
-      return (
-        <div>
-            <p>5Display for someHashValue</p>
-            <p>가심비 - 관광지 마스터 - 뚜벅이</p>
-        </div>
-      )
-    } else if (hash === "997a63628720da65f8cf6c40b791ca72") {
-      return (
-        <div>
-            <p>6Display for someHashValue</p>
-            <p>가심비 - 관광지 마스터 - 드라이브</p>
-        </div>
-      )
-    } else if (hash === "043cdb832a8382605fbace17a5501fdf") {
-      return (
-        <div>
-            <p>7Display for someHashValue</p>
-            <p>가심비 - 숙소 올인형 - 뚜벅이</p>
-        </div>
-      )
-    } else if (hash === "acbf415938afff17a73812981202864a") {
-      return (
-        <div>
-            <p>8Display for someHashValue</p>
-            <p>가심비 - 숙소 올인형 - 드라이브</p>
-        </div>
-      )
+    if (hash === "5e901317337e58ac4695c38adf172bd1") {      
+      firResultImg = resultImg[0][0];
+      secResultImg = resultImg[1][0];
+      trdResultImg = resultImg[2][0];
+      firResultText = "가성비";
+      secResultText = "관광지 마스터";
+      trdResultText = "뚜벅이";         
+    } else if (hash === "83873ecd67c7cef8aad8f4b8169f8579") {      
+      firResultImg = resultImg[0][0];
+      secResultImg = resultImg[1][0];
+      trdResultImg = resultImg[2][1];
+      firResultText = "가성비";
+      secResultText = "관광지 마스터";
+      trdResultText = "드라이브";
+    } else if (hash === "8d4328c8b65023e4551acae6071d82fd") {      
+      firResultImg = resultImg[0][0];
+      secResultImg = resultImg[1][1];
+      trdResultImg = resultImg[2][0];
+      firResultText = "가성비";
+      secResultText = "숙소 올인형";
+      trdResultText = "뚜벅이";
+    } else if (hash === "5b40c94e1382312acb1b856e64759e19") {      
+      firResultImg = resultImg[0][0];
+      secResultImg = resultImg[1][1];
+      trdResultImg = resultImg[2][1];
+      firResultText = "가성비";
+      secResultText = "숙소 올인형";
+      trdResultText = "드라이브";
+    } else if (hash === "d938c442280818e9d3ca7808d3b65bd5") {      
+      firResultImg = resultImg[0][1];
+      secResultImg = resultImg[1][0];
+      trdResultImg = resultImg[2][0];
+      firResultText = "가심비";
+      secResultText = "관광지 마스터";
+      trdResultText = "뚜벅이";
+    } else if (hash === "997a63628720da65f8cf6c40b791ca72") {      
+      firResultImg = resultImg[0][1];
+      secResultImg = resultImg[1][0];
+      trdResultImg = resultImg[2][1];
+      firResultText = "가심비";
+      secResultText = "관광지 마스터";
+      trdResultText = "드라이브";
+    } else if (hash === "043cdb832a8382605fbace17a5501fdf") {      
+      firResultImg = resultImg[0][1];
+      secResultImg = resultImg[1][1];
+      trdResultImg = resultImg[2][0];
+      firResultText = "가심비";
+      secResultText = "숙소 올인형";
+      trdResultText = "뚜벅이";
+    } else if (hash === "acbf415938afff17a73812981202864a") {      
+      firResultImg = resultImg[0][1];
+      secResultImg = resultImg[1][1];
+      trdResultImg = resultImg[2][1];
+      firResultText = "가심비";
+      secResultText = "숙소 올인형";
+      trdResultText = "드라이브";
     }
-
-    // ... handle other hash values
-
-    return <div>Default Display</div>;
+    return (
+        <div style={styles.centerContainer}>
+            <h1>나의 KQ 숙소 유형 키워드</h1>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                    m: 1,
+                    width: 240,
+                    height: 360,
+                    },
+                }}
+            >
+                <Card>
+                    <CardMedia
+                        sx={{ height: 240 }}
+                        image= {firResultImg}
+                        title="green iguana"
+                    />
+                    <CardContent
+                        style ={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            height: '100%',
+                        }}
+                    >
+                        <Typography>
+                            <h2>{firResultText}</h2>
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardMedia
+                        sx={{ height: 240 }}
+                        image= {secResultImg}
+                        title="green iguana"
+                    />
+                    <CardContent
+                        style ={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            height: '100%',
+                        }}
+                    >
+                        <Typography>
+                            <h2>{secResultText}</h2>
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardMedia
+                        sx={{ height: 240 }}
+                        image= {trdResultImg}
+                        title="green iguana"
+                    />
+                    <CardContent
+                        style ={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            height: '100%',
+                        }}
+                    >
+                        <Typography>
+                            <h2>{trdResultText}</h2>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Box>
+            <Stack>여수, 경주, 서울 등 국내 주요 관광지 근처의</Stack>
+            <Stack>접근성 좋은 게스트 하우스를 추천해요!</Stack>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                    m: 1,
+                    width: 300,
+                    height: 400,
+                    },
+                }}
+            >
+                <Card>
+                    <CardMedia
+                        sx={{ height: 300 }}
+                        image= {H1}
+                        title="green iguana"
+                    />
+                    <CardContent
+                        style ={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            height: '100%',
+                        }}
+                    >
+                        <Typography>
+                            <h2>유리트리트</h2>
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardMedia
+                        sx={{ height: 300 }}
+                        image= {H2}
+                        title="green iguana"
+                    />
+                    <CardContent
+                        style ={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            height: '100%',
+                        }}
+                    >
+                        <Typography>
+                            <h2>힐튼 스테이 코스모스</h2>
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardMedia
+                        sx={{ height: 300 }}
+                        image= {H3}
+                        title="green iguana"
+                    />
+                    <CardContent
+                        style ={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            height: '100%',
+                        }}
+                    >
+                        <Typography>
+                            <h2>포트 엔 포트</h2>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Box>
+            <Stack>*사진을 누르면 숙소 소개 페이지로 이동합니다.</Stack>
+            <Button 
+                variant="contained"
+                size="large"
+            >
+                이벤트 응모하기
+            </Button>
+            <Link to="/">홈으로 돌아가기</Link>
+        </div>        
+    )
   }
 }
 
@@ -80,5 +257,13 @@ const ResultScreenWrapper = () => {
   const { hash } = useParams() as { hash: string }; // Type assertion here
   return <ResultScreen hash={hash} />;
 };
-
+const styles: { centerContainer: CSSProperties } = {
+  centerContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+  },
+};
 export default ResultScreenWrapper;
